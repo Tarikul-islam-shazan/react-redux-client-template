d517ccfc676394f3d8fcebe15126499b12de5de5
khan_shazid_jt <obydulislam.khan@japantravel.com>
2020-11-16 23:36:07 +0600

    button name renamed

--- src/app/pages/design/OurDesignDetails.js
+++ src/app/pages/design/OurDesignDetails.js
@@ -189,8 +189,8 @@ class OurDesignDetails extends Component {
                                   <div className="text-left mt-5">
                                     {
                                       product.availabilityStatus !== 'AVAILABLE' ?
-                                      <button className="btn btn-outline-secondary" disabled={true}>Ask for quotation</button> :
-                                      <a href="" className="btn btn-nitex-default" data-toggle="modal" data-target="#quickQuoteModal" onClick={() => this.props._storeData('choosenIdsForQuick',[this.props.match.params.id])}>Ask for quotation</a>
+                                      <button className="btn btn-outline-secondary" disabled={true}>Quote now</button> :
+                                      <a href="" className="btn btn-nitex-default" data-toggle="modal" data-target="#quickQuoteModal" onClick={() => this.props._storeData('choosenIdsForQuick',[this.props.match.params.id])}>Quote now</a>
                                     }
                                       {/*<a href="" className="btn btn-nitex-default" data-toggle="modal" data-target="#quickProjectModal" onClick={() => this.props._storeData('choosenIdsForQuick',[this.props.match.params.id])}>Start Project</a>*/}
                                   </div>
