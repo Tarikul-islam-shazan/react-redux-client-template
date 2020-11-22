6be39aaeeaf1ea32f6d7e7635a8308e8c4614b62
khan_shazid_jt <obydulislam.khan@japantravel.com>
2020-11-22 20:11:23 +0600

    new package added for input

--- src/app/pages/questionairre/Questionairre.js
+++ src/app/pages/questionairre/Questionairre.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import loadjs from "loadjs";
+import IntlTelInput from 'react-intl-tel-input';
+import 'react-intl-tel-input/dist/main.css';
 
 import { _storeData } from "./actions";
 
@@ -12,25 +15,82 @@ import { QuestionnaireOption } from './components/OptionCardComponent';
 
 class Questionairre_1 extends Component {
 
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          role: '',
+          label: '',
+          phoneNumber: '',
+          countryCode: '',
+          roleError: '',
+          labelError: '',
+          phoneNumberError: '',
+          countryCodeError: '',
+        };
+    }
+
     componentDidMount = async() => {
 
     }
 
-    _setData = (profession) => {
-      console.log("clicked",profession)
-      this.props._storeData("profession",profession)
-    }
-
-    _next = (profession) => {
-      this.props.history.push('/questionairre-step-2');
-    }
-
     onChange = (e) => {
-      this.props._storeData(e.target.name,e.target.value)
+        this.setState({
+          [e.target.name]: e.target.value
+        })
+    }
+
+    onChangeNumber = (e, f, g) => {
+      console.log("onChangeNumber", e, f, g)
+    }
+
+    _validate = async() => {
+        let flag = true;
+        let { role, label, phoneNumber, countryCode } = this.state;
+        if (!role) {
+            flag = false;
+            await this.setState({
+                roleError: 'Role is required!'
+            })
+        } else {
+            await this.setState({
+                roleError: ''
+            })
+        }
+        if (!label) {
+            flag = false;
+            await this.setState({
+                labelError: 'Label status is required!'
+            })
+        } else {
+            await this.setState({
+                labelError: ''
+            })
+        }
+        if (!phoneNumber) {
+            flag = false;
+            await this.setState({
+                roleError: 'Phone number is required!'
+            })
+        } else {
+            await this.setState({
+                roleError: ''
+            })
+        }
+        loadjs(['/js/script.js']);
+        return flag;
+    }
+
+    _submit = async() => {
+        let { role, label, phoneNumber, countryCode, roleError, labelError, phoneNumberError, countryCodeError } = this.state;
+        if (this._validate()) {
+
+        }
+
     }
 
     render() {
-        let { professionText } = this.props;
+        let { role, label, phoneNumber, countryCode, roleError, labelError, phoneNumberError, countryCodeError } = this.state;
         return (
             <div className="questionnaire">
                 <div className="questionnaire-form">
@@ -41,8 +101,8 @@ class Questionairre_1 extends Component {
                     <div className="row">
                         <div className="col-lg-12">
                             <div className="form-group">
-                                <label for="">What is your role in business (Select one)?</label>
-                                <select>
+                                <label htmlFor="">What is your role in business (Select one)?</label>
+                                <select className={labelError ? 'error' : ''} name="role" value={role} onClick={this.onChange}>
                                     <option value="">Please choose one…</option>
                                     <option value="OWNER">Owner</option>
                                     <option value="DIRECTOR">Director</option>
@@ -51,11 +111,15 @@ class Questionairre_1 extends Component {
                                     <option value="BUYER">Buyer</option>
                                     <option value="OTHER">Others</option>
                                 </select>
+                                {
+                                  roleError ?
+                                  <div className="error">{roleError}</div>:<></>
+                                }
                             </div>
                             <div className="form-group">
                                 <span className="info">We help private labels, wholesalers, & retailers to manufacture in bulk</span>
-                                <label for="">Do you have a private clothing label?</label>
-                                <select className="error">
+                                <label htmlFor="">Do you have a private clothing label?</label>
+                                <select className={labelError ? 'error' : ''} name="label" value={label} onClick={this.onChange}>
                                     <option value="">Please choose one…</option>
                                     <option value="DONT_HAVE_LABEL_YET">No! Don't have a label yet</option>
                                     <option value="DO_DROPSHIP_ONLY">We do drop-shipping only</option>
@@ -63,18 +127,29 @@ class Questionairre_1 extends Component {
                                     <option value="SELL_WHOLESALE_TO_RETAILERS">We sell wholesale to retailers</option>
                                     <option value="MAKE_PROMOTIONAL_GOODS">We make promotional goods</option>
                                 </select>
-                                <div className="error">Please fill out this information related demo content</div>
+                                {
+                                  labelError ?
+                                  <div className="error">{labelError}</div>:<></>
+                                }
                             </div>
                             <div className="form-group">
                                 <span className="info">Our business evangelist will call you to cater for your needs</span>
-                                <label for="">How can we contact you?</label>
+                                <label htmlFor="">How can we contact you?</label>
+                                <IntlTelInput
+                                  containerClassName="intl-tel-input"
+                                  inputClassName="form-control"
+                                  // value={phoneNumber}
+                                  onSelectFlag={this.onChangeNumber}
+                                  onPhoneNumberChange={this.onChangeNumber}
+                                  // onChange={(e) => this.onChangeNumber(e)}
+                                />
                                 <div className="country-code">
                                     <input id="phone" type="tel"/>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <button className="btn-brand">Enter</button>
+                    <button className="btn-brand" onClick={this._submit}>Enter</button>
                 </div>
             </div>
         );
