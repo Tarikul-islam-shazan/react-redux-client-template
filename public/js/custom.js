efe336e460eb926e0a74ee0e1cc23a6aaccfaed2
sultan0 <sultanmahmud0@gamil.com>
2021-01-01 19:54:29 +0600

    quotes udpate

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,57 +1,114 @@
 // Collapse Sidebar Menu
 (function($) {
 
-	"use strict";
+    "use strict";
 
-	var fullHeight = function() {
+    var fullHeight = function() {
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function(){
+            $('.js-fullheight').css('height', $(window).height());
+        });
 
-	};
-	fullHeight();
+    };
+    fullHeight();
+    // Product Carousel
+    $('.Product-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        dots: false,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            1024:{
+                items:3,
+                nav:false
+            },
+            1025:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+    $('.Product-carousel-related').owlCarousel({
+        loop:true,
+        margin:20,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        dots: false,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            991:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    })
+    $('.explore-design-carasoul').owlCarousel({
+        loop:true,
+        margin:35,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        dots: false,
+        autoplayTimeout:2500,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            768:{
+                items:2,
+                nav:false
+            },
+            1024:{
+                items:3,
+                nav:false
+            },
+            1280:{
+                items:4,
+                nav:true,
+                loop:false
+            },
+            1920:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    })
 
+    $(document).ready(function(){
+        $('#zoom').zoom();
+    });
+
+    $('#catItem').click(function(){
+        $('.filter-cat').toggle("slow");
+    });
+    $('.close').click(function(){
+        $('.selected-item-popup').addClass('hide');
+    });
+    $('.add-more-design').click(function(){
+        $('.add-more').addClass('open');
+    });
+    $('#closeRPop').click(function(){
+        $('.add-more').removeClass('open');
+    });
 })(jQuery);
 
 
-// Product Carousel
-$('.Product-carousel').owlCarousel({
-    loop:true,
-    margin:20,
-	responsiveClass:true,
-	autoplay:true,
-	autoplayHoverPause:true,
-	dots: false,
-    nav:true,
-    items : 1, // THIS IS IMPORTANT
-    responsive : {
-        767 : { items : 1  },
-        768 : { items : 3  },
-        1280 : { items : 4
-        }
-    },
-});
-$('.Product-carousel-related').owlCarousel({
-    loop:true,
-    margin:20,
-	responsiveClass:true,
-	autoplay:true,
-	autoplayHoverPause:true,
-	dots: false,
-    nav:true,
-    items : 1, // THIS IS IMPORTANT
-    responsive : {
-        767 : { items : 1  },
-        768 : { items : 3  },
-        1280 : { items : 4
-        }
-    },
-});
-
-$(document).ready(function(){
-    $('#zoom').zoom();
-});
-
-// Toggle Responsive Menu
