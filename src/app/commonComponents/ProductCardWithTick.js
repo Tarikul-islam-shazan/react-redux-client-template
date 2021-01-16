497e3fcf9b70cbc8310dc11775700fc9ed452711
sultan0 <sultanmahmud0@gamil.com>
2021-01-16 13:41:47 +0600

    card submenu update

--- src/app/commonComponents/ProductCardWithTick.js
+++ src/app/commonComponents/ProductCardWithTick.js
@@ -66,10 +66,33 @@ class ProductCard extends Component {
                           <path id="Path_27721" data-name="Path 27721" d="M164.573,353.29l3.281,3.949,12.212-12.212" transform="translate(-161.757 -342.198)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"/>
                       </svg>
                   </div>
-                  <div className={`favourite-part ${product.liked ? 'active' : ''}`} onClick={() => this.toggleLike(product.id)}>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="14.521" height="13.594" viewBox="0 0 14.521 13.594">
-                          <path id="like_1_" data-name="like (1)" d="M14.5,5.88a4.13,4.13,0,0,0-3.93-4.053A3.9,3.9,0,0,0,7.221,3.768,3.761,3.761,0,0,0,3.954,1.826,4.13,4.13,0,0,0,.024,5.879,4.207,4.207,0,0,0,.148,7.42,6.615,6.615,0,0,0,2.158,10.8L7.217,15.42,12.363,10.8a6.616,6.616,0,0,0,2.01-3.378A4.217,4.217,0,0,0,14.5,5.88Z" transform="translate(0 -1.826)" fill="#9098ac"/>
+                  {/*<div className={`favourite-part ${product.liked ? 'active' : ''}`} onClick={() => this.toggleLike(product.id)}>*/}
+                  {/*    <svg xmlns="http://www.w3.org/2000/svg" width="14.521" height="13.594" viewBox="0 0 14.521 13.594">*/}
+                  {/*        <path id="like_1_" data-name="like (1)" d="M14.5,5.88a4.13,4.13,0,0,0-3.93-4.053A3.9,3.9,0,0,0,7.221,3.768,3.761,3.761,0,0,0,3.954,1.826,4.13,4.13,0,0,0,.024,5.879,4.207,4.207,0,0,0,.148,7.42,6.615,6.615,0,0,0,2.158,10.8L7.217,15.42,12.363,10.8a6.616,6.616,0,0,0,2.01-3.378A4.217,4.217,0,0,0,14.5,5.88Z" transform="translate(0 -1.826)" fill="#9098ac"/>*/}
+                  {/*    </svg>*/}
+                  {/*</div>*/}
+                  <div className="card-options dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="27" height="6" viewBox="0 0 27 6">
+                          <g id="Group_10" data-name="Group 10" transform="translate(-4045 -1237) rotate(-90)">
+                              <path id="Path_27893" data-name="Path 27893" d="M22.5,19.5a3,3,0,1,1-3-3A3,3,0,0,1,22.5,19.5Z" transform="translate(-1259.5 4039)" fill="#cbcbcb"/>
+                              <path id="Path_27894" data-name="Path 27894" d="M22.5,9a3,3,0,1,1-3-3A3,3,0,0,1,22.5,9Z" transform="translate(-1259.5 4039)" fill="#cbcbcb"/>
+                              <path id="Path_27895" data-name="Path 27895" d="M22.5,30a3,3,0,1,1-3-3A3,3,0,0,1,22.5,30Z" transform="translate(-1259.5 4039)" fill="#cbcbcb"/>
+                          </g>
                       </svg>
+                      <div>
+                          <ul className="dropdown-menu dropdown-menu-right shadow-lg mt-3" role="menu"
+                              aria-labelledby="menu1">
+                              <li role="presentation" className="px-4 pb-3 pt-3">
+                                  <a role="menuitem" tabIndex="-1" href="#" className="font-weight-normal  text-black">Like this design</a>
+                              </li>
+                              <li role="presentation" className="px-4 pb-3">
+                                  <a role="menuitem" tabIndex="-1" href="#" className="font-weight-normal  text-black">Add to quote</a>
+                              </li>
+                              <li role="presentation" className="px-4 pb-3">
+                                  <a role="menuitem" tabIndex="-1" href="#" className="font-weight-normal  text-black">Add to collection</a>
+                              </li>
+                          </ul>
+                      </div>
                   </div>
                   {
                     product.designDocuments.length > 0 ?
