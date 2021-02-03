6dcb324676d09eb9891bb969f137709c6f0eafca
MacBook Pro <hello@MacBooks-MacBook-Pro.local>
2021-02-03 21:54:29 +0600

    quote now updated according to new data format

--- src/app/pages/design/actions/index.js
+++ src/app/pages/design/actions/index.js
@@ -1,53 +1,6 @@
 import axios from "axios";
 import Http from '../../../services/Http';
 
-const DUMMY = [
-    {
-        "fabricComposition": "60% Cotton 40% Polyester (CVC)",
-        "fabricWeight": "270",
-        "documentResponseList": [
-            {
-                "id": 5792,
-                "docType": "REFERENCE_IMAGE",
-                "docUrl": "https://d2939dhdpmjcbe.cloudfront.net/product/reference_image/2020/9/1599315684669_HOODIE_WITH_BACK_PRINT-(MT346-SS-21-BACK).png",
-                "name": "1599315684669_HOODIE_WITH_BACK_PRINT-(MT346-SS-21-BACK).png",
-                "print": false
-            },
-            {
-                "id": 5793,
-                "docType": "PRODUCT_DESIGN",
-                "docUrl": "https://d2939dhdpmjcbe.cloudfront.net/product/design/2020/9/1599315684559_HOODIE_WITH_BACK_PRINT-(MT346-SS-21-FRONT).png",
-                "name": "1599315684559_HOODIE_WITH_BACK_PRINT-(MT346-SS-21-FRONT).png",
-                "print": false
-            }
-        ],
-        "productType": "Hoodie",
-        "productGroup": "Men"
-    },
-    {
-        "fabricComposition": "100% Cotton Pique",
-        "fabricWeight": "190",
-        "documentResponseList": [
-            {
-                "id": 5787,
-                "docType": "PRODUCT_DESIGN",
-                "docUrl": "https://d2939dhdpmjcbe.cloudfront.net/product/design/2020/9/1599315596789_FLARED_CROP_TOP-(WT267-SS-21-FRONT).png",
-                "name": "1599315596789_FLARED_CROP_TOP-(WT267-SS-21-FRONT).png",
-                "print": false
-            },
-            {
-                "id": 5786,
-                "docType": "REFERENCE_IMAGE",
-                "docUrl": "https://d2939dhdpmjcbe.cloudfront.net/product/reference_image/2020/9/1599315596914_FLARED_CROP_TOP-(WT267-SS-21-BACK).png",
-                "name": "1599315596914_FLARED_CROP_TOP-(WT267-SS-21-BACK).png",
-                "print": false
-            }
-        ],
-        "productType": "Crop top",
-        "productGroup": "Women"
-    }
-];
-
 const SAMPLE_SIZE_DATA = [
                 {
                     "code": "XS", //size code
@@ -87,9 +40,16 @@ export const _getProductForQuote = async(productIds) => {
 			.then(({data}) => {
 						if (data.length) {
 								data.map((product) => {
-									if (!product.sizeQuantityPairList) {
-										product.sizeQuantityPairList = SAMPLE_SIZE_DATA;
-									}
+                  if (product.colors) {
+                    product.colorWiseSizeQuantityPairList = product.colors.map((color) => {
+                      if (product.sizeQuantityPairList) {
+                        color.sizeQuantityPairList = product.sizeQuantityPairList;
+                      } else {
+                        color.sizeQuantityPairList = SAMPLE_SIZE_DATA;
+                      }
+                      return color;
+                    })
+                  }
                   result.push(product);
 								})
 						}
