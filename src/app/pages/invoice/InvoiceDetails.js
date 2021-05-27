c457f788dde0062fe3f1280c09699171c9ce876b
unknown <rana.nitex@gmail.com>
2021-05-27 18:05:18 +0600

    make functional invoice details page

--- src/app/pages/invoice/InvoiceDetails.js
+++ src/app/pages/invoice/InvoiceDetails.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import BootstrapTable from 'react-bootstrap-table-next';
-import axios from 'axios';
-import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import loadjs from 'loadjs';
-
 import LoadingOverlay from 'react-loading-overlay';
-import Http from '../../services/Http';
-import { toastSuccess, toastError, toastWarning } from '../../commonComponents/Toast';
-import ProductCard from '../../commonComponents/ProductCard';
-import { encodeQueryData, _getKey, invoiceStatus, changeDateFormat } from '../../services/Util';
+
+import { invoiceStatus, changeDateFormat } from '../../services/Util';
 import { LOADER_OVERLAY_BACKGROUND, LOADER_COLOR, LOADER_WIDTH, LOADER_TEXT, LOADER_POSITION, LOADER_TOP, LOADER_LEFT, LOADER_MARGIN_TOP, LOADER_MARGIN_LEFT } from '../../constant';
 import { getDetails } from './actions';
-
-
-// import {InvoiceItem} from './components/InvoiceItem';
 
 class InvoiceDetails extends Component {
 
@@ -24,7 +14,7 @@ class InvoiceDetails extends Component {
         this.state = {
           invoice: {},
           loading: false,
-          isDownload: false
+          isDownload: false,
         };
     }
 
@@ -36,29 +26,6 @@ class InvoiceDetails extends Component {
       await getDetails(id)
         .then((data) => this.setState({ invoice: data }))
         .finally(() => this.setState({ loading: false }))
-    //   let id = this.props.match.params.id;
-    //   await this.getDetails(id);
-    // }
-
-    // getDetails = async(id) => {
-    //   await this.setState({loading: true});
-
-    //   await Http.GET('getInvoiceDetails', id)
-    //     .then(({data}) => {
-    //       console.log('invoiceList SUCCESS: ', data);
-    //       data.toAddress = data.toAddress ? JSON.parse(data.toAddress) : data.toAddress;
-    //       data.fromAddress = data.fromAddress ? JSON.parse(data.fromAddress) : data.fromAddress;
-    //       this.setState({loading: false, invoice: data})
-    //     })
-    //     .catch(({response}) => {
-    //         console.log('invoiceList ERROR: ', response);
-    //         this.setState({loading: false})
-    //         if (response && response.data && response.data.message) {
-    //           toastError(response.data.message);
-    //         } else {
-    //           toastError("Something went wrong! Please try again.");
-    //         }
-    //     });
     }
 
     componentWillUnmount = () => {
@@ -74,12 +41,44 @@ class InvoiceDetails extends Component {
     }
 
     render() {
-        let {invoice} = this.state;
+        let {invoice, isDownload} = this.state;
         let toAddress = invoice.toAddress ? invoice.toAddress.address : {};
-        let fromAddress = invoice.fromAddress ? invoice.fromAddress.address : {};
-        console.log('---->', invoice);
+        let fromAddress = invoice.fromAddress ? invoice.fromAddress : {};
+        console.log('NOTES--SSSSSS', invoice);
+        const onDownload = (id) => {
+          return (
+            this.setState({
+              isDownload: true
+            })
+          )
+        }
         return (
-          <>
+          <LoadingOverlay
+          active={this.state.loading}
+          styles={{
+            overlay: (base) => ({
+              ...base,
+              background: LOADER_OVERLAY_BACKGROUND
+            }),
+            spinner: (base) => ({
+              ...base,
+              width: LOADER_WIDTH,
+              position: LOADER_POSITION,
+              top: LOADER_TOP,
+              left: LOADER_LEFT,
+              marginTop: LOADER_MARGIN_TOP,
+              marginLeft: LOADER_MARGIN_LEFT,
+              '& svg circle': {
+                stroke: LOADER_COLOR
+              }
+            }),
+            content: (base) => ({
+              ...base,
+              color: LOADER_COLOR
+            })
+          }}
+          spinner
+          text={LOADER_TEXT}>
             <div class="back cursor-pointer" onClick={() => this.props.history.push('/invoices/list')}></div>
 
             <div class="admin-view-invoice mt-4">
@@ -96,7 +95,7 @@ class InvoiceDetails extends Component {
                         <div class="text-center mr-0 mr-sm-5">
                             {invoiceStatus(invoice)}
                         </div>
-                        <button class="btn-border my-3">Download</button>
+                        <button onClick={() => onDownload(invoice.id)} className="btn-border my-3">Download</button>
                         <button onClick={() => this.onPayInvoice(invoice.id)}class="btn-border brand-bg-color text-white border-0 px-5 ml-0 ml-sm-3">Pay</button>
                     </div>
                 </div>
@@ -156,18 +155,6 @@ class InvoiceDetails extends Component {
                                               <span>Total price</span> <br/>
                                               <strong>${item.price}</strong>
                                           </td>
-                                          {/*<td>
-                                              <span class="cursor-pointer">
-                                                  <svg xmlns="http://www.w3.org/2000/svg" width="20.27" height="24.962" viewBox="0 0 20.27 24.962">
-                                                    <g id="delete" transform="translate(0)">
-                                                      <path id="Path_27884" data-name="Path 27884" d="M222.983,154.7a.585.585,0,0,0-.585.585v11.051a.585.585,0,0,0,1.169,0V155.288a.585.585,0,0,0-.584-.588Zm0,0" transform="translate(-209.397 -145.659)" fill="red"/>
-                                                      <path id="Path_27885" data-name="Path 27885" d="M104.983,154.7a.585.585,0,0,0-.585.585v11.051a.585.585,0,0,0,1.169,0V155.288a.585.585,0,0,0-.584-.588Zm0,0" transform="translate(-98.297 -145.659)" fill="red"/>
-                                                      <path id="Path_27886" data-name="Path 27886" d="M1.657,7.431V21.837a3.226,3.226,0,0,0,.858,2.225,2.879,2.879,0,0,0,2.089.9H15.667a2.879,2.879,0,0,0,2.089-.9,3.226,3.226,0,0,0,.858-2.225V7.431a2.234,2.234,0,0,0-.573-4.392H15.047V2.308A2.3,2.3,0,0,0,12.732,0H7.54A2.3,2.3,0,0,0,5.224,2.31v.731H2.23a2.234,2.234,0,0,0-.573,4.392ZM15.667,23.8H4.6a1.852,1.852,0,0,1-1.777-1.959V7.483H17.444V21.837A1.852,1.852,0,0,1,15.667,23.8ZM6.393,2.308A1.126,1.126,0,0,1,7.54,1.168h5.192a1.126,1.126,0,0,1,1.146,1.14v.731H6.393ZM2.23,4.208H18.04a1.052,1.052,0,0,1,0,2.1H2.23a1.052,1.052,0,1,1,0-2.1Zm0,0" transform="translate(0 0)" fill="red"/>
-                                                      <path id="Path_27887" data-name="Path 27887" d="M163.983,154.7a.585.585,0,0,0-.585.585v11.051a.585.585,0,1,0,1.169,0V155.288a.585.585,0,0,0-.584-.588Zm0,0" transform="translate(-153.847 -145.659)" fill="red"/>
-                                                    </g>
-                                                  </svg>
-                                              </span>
-                                          </td>*/}
                                       </tr>
                                     )
                                   }) : <></>
@@ -180,11 +167,11 @@ class InvoiceDetails extends Component {
                     <div class="row mt-5">
                         <div class="col-sm-12 col-lg-6">
                             <div class="form-group">
-                                <label class="semibold">NOTES</label>
+                                <label class="semibold">Notes</label>
                                 <article class="invoice-note">
-                                    <div dangerouslySetInnerHTML={{__html: invoice.note}}>
-
-                                    </div>
+                                    <p>
+                                        {invoice.note}
+                                    </p>
                                 </article>
                             </div>
                         </div>
@@ -209,18 +196,10 @@ class InvoiceDetails extends Component {
                             </table>
                         </div>
                     </div>
-
-                    {/* <div class="row mt-2 mt-sm-5">
-                        <div class="col-md-12">
-                            <div class="text-right">
-                                <button class="btn-brand m-0">Approve</button>
-                            </div>
-                        </div>
-                    </div> */}
                 </div>
-
             </div>
-          </>
+           
+            </LoadingOverlay>
         );
     }
 }
