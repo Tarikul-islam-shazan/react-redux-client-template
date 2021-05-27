f48dfef4fb2762cacf5b941e6bac6c00d1dc1911
unknown <rana.nitex@gmail.com>
2021-05-27 15:50:38 +0600

    implent PRODUCT_SOLD and  and ORDER_PLACED status and change sorting accordingly

--- src/app/pages/rfo/MyRFQsV2.js
+++ src/app/pages/rfo/MyRFQsV2.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import BootstrapTable from 'react-bootstrap-table-next';
-import axios from 'axios';
-import { Redirect, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import loadjs from 'loadjs';
-import Modal from 'react-bootstrap/Modal'
+import LoadingOverlay from 'react-loading-overlay';
 
 import { _storeData } from "../design/actions";
-
-import LoadingOverlay from 'react-loading-overlay';
 import Http from '../../services/Http';
-import { rfqStatus, rfqProductStatus, convertTimeToLocal, changeDateFormat, authUser } from '../../services/Util';
-
+import { changeDateFormat, authUser } from '../../services/Util';
 import { toastSuccess, toastError, toastWarning } from '../../commonComponents/Toast';
-import { RfqCard } from './components/RfqCard';
-import {RfqSkeleton, RfqProductSkeleton, CreateSkeletons} from '../../commonComponents/ProductSkeleton';
 import {QuotedItem} from './components/QuotedItem';
-
 import { LOADER_OVERLAY_BACKGROUND, LOADER_COLOR, LOADER_WIDTH, LOADER_TEXT, LOADER_POSITION, LOADER_TOP, LOADER_LEFT, LOADER_MARGIN_TOP, LOADER_MARGIN_LEFT } from '../../constant';
 import EmptyState from '../../commonComponents/EmptyState';
 
@@ -118,27 +109,21 @@ class MyRFQs extends Component {
               this.renderList(page + 1, true);
           } else {
               if (!hasNext) {
-                  // toastWarning("No more data found.")
+                  toastWarning("No more data is available.")
               }
           }
-          // this.setState({
-          //     message: 'bottom reached'
-          // });
       } else {
       }
   };
 
   renderList = async (page = 0, merge = false) => {
     this.setState({ loading: true })
-    let { size, rfqList, search, filterBy, sort, filterById, status, date, collection } = this.state;
-    // let params = `?page=${page}&size=${size}`;
+    let { size, rfqList, search, sort, status, date, collection } = this.state;
     let params = {
       page,
       size,
       search,
-      // filterBy : filterBy,
       sort,
-      // id: filterById,
       status,
       date: date
         ? changeDateFormat(date, "YYYY/MM/DD", "DD/MM/YYYY")
@@ -193,7 +178,6 @@ class MyRFQs extends Component {
         }
           // toastWarning("RFQ List - no data found.");
         }
-        // loadjs(['/js/script.js', '/js/custom.js']);
       })
       .catch(response => {
         this.setState({ loading: false, productLoading: false })
@@ -226,11 +210,15 @@ class MyRFQs extends Component {
   toggleSelect = async(e) => {
     let {rfqList, allCheck} = this.state;
     rfqList = rfqList.map((rfq, i) => {
+      console.log('!!!!!RFQQQ', rfq);
       if (rfq.id == e.target.value) {
         rfq.isSelected = e.target.checked;
       }
       if (e.target.name === 'all') {
         rfq.isSelected = e.target.checked;
+      }
+      if(rfq.status === "ORDER_PLACED" || rfq.status === "PRODUCT_SOLD"){
+        rfq.isSelected = false
       }
       return rfq;
     })
@@ -311,7 +299,7 @@ class MyRFQs extends Component {
   }
 
   render() {
-    let { rfqList, rfqDetails, showNegotiation, messages, userInfo, message, sort, ids, selectedProductName, filterById, hasNext, allCheck, total, search, status, date, totalSelectedItems, collection, orderTitle, orderFlag } = this.state;
+    let { rfqList, sort, hasNext, allCheck, total, search, status, date, totalSelectedItems, collection, orderTitle, orderFlag } = this.state;
     if (!hasNext && !rfqList.length) {
       return (
         <div className="mt-5 not-found">
@@ -431,7 +419,13 @@ class MyRFQs extends Component {
             {
               rfqList.map((quote, i) =>{
                 return(
-                  <QuotedItem quote={quote} key={i} index={i} toggleSelect={this.toggleSelect} search={this.searchByCollection} authUserInfo={userInfo}/>
+                  <QuotedItem 
+                    quote={quote} 
+                    key={i} 
+                    index={i} 
+                    toggleSelect={this.toggleSelect} 
+                    search={this.searchByCollection}
+                  />
                 )
               })
             }
--- src/app/pages/rfo/components/QuotedItem.js
+++ src/app/pages/rfo/components/QuotedItem.js
@@ -3,8 +3,7 @@ import {addImageSuffix, rfqProductStatus, convertTimeToLocal, changeDateFormat} 
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 
-export const QuotedItem = ({quote, index, toggleSelect, search, authUserInfo
-}) => {
+export const QuotedItem = ({quote, index, toggleSelect, search}) => {
   
   let flag = 1;
   let timeDifference = 0;
@@ -27,13 +26,56 @@ export const QuotedItem = ({quote, index, toggleSelect, search, authUserInfo
     document.getElementsByClassName('IconLauncher__BaseLauncher-m649nu-0 IconLauncher__CircleLauncher-m649nu-2 eFiccJ reagan--widget-loaded undefined')[0].click()
   }
 
+  const renderTollTip = (message) => {
+      return(
+          <svg className="ml-2 cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16" title={message} data-toggle="tooltip" data-placement="top">
+          <g id="Group_13" data-name="Group 13" transform="translate(1445 -3504)">
+            <circle id="Ellipse_1" data-name="Ellipse 1" cx="7" cy="7" r="7" transform="translate(-1445 3506)" fill="#ddd"/>
+            <text id="Price_will_be_updated_within_24_hours" data-name="Price will be updated within 24 hours" transform="translate(-1438 3516)" fill="#21242b" font-size="11" font-family="SegoeUI, Segoe UI"><tspan x="-1.332" y="0">i</tspan></text>
+          </g>
+        </svg>
+      )
+  }
+
+  const renderPriceUpdateBox = (quote) => {
+    return(
+      <div className="pricewillbeupdated pt-2 pb-3">
+          <svg xmlns="http://www.w3.org/2000/svg" width="211" height="54" viewBox="0 0 211 54">
+              <g id="Group_11081" data-name="Group 11081" transform="translate(-1549 -407)">
+                  <g id="Rectangle_6056" data-name="Rectangle 6056" transform="translate(1549 407)" fill="#f4f5f7">
+                      <path d="M 204 53.5 L 7 53.5 C 3.415894746780396 53.5 0.5 50.5841064453125 0.5 47 L 0.5 7 C 0.5 3.415894746780396 3.415894746780396 0.5 7 0.5 L 204 0.5 C 207.5841064453125 0.5 210.5 3.415894746780396 210.5 7 L 210.5 47 C 210.5 50.5841064453125 207.5841064453125 53.5 204 53.5 Z" stroke="none"/>
+                      <path d="M 7 1 C 3.69158935546875 1 1 3.691581726074219 1 7 L 1 47 C 1 50.30841827392578 3.69158935546875 53 7 53 L 204 53 C 207.3084106445313 53 210 50.30841827392578 210 47 L 210 7 C 210 3.691581726074219 207.3084106445313 1 204 1 L 7 1 M 7 0 L 204 0 C 207.8659973144531 0 211 3.133998870849609 211 7 L 211 47 C 211 50.86600112915039 207.8659973144531 54 204 54 L 7 54 C 3.134002685546875 54 0 50.86600112915039 0 47 L 0 7 C 0 3.133998870849609 3.134002685546875 0 7 0 Z" stroke="none" fill="#edeff4"/>
+                  </g>
+                  <text id="_-----------" data-name="-----------" transform="translate(1604 441)" fill="#aaacaf" font-size="20" font-family="OpenSans, Open Sans" letter-spacing="0.03em"><tspan x="0" y="0">{quote.price ? quote.price : `—————`}</tspan></text>
+              </g>
+          </svg>
+      </div>
+    )
+  } 
+const renderDisscussButton = () => {
+  return (
+    <div className="position-relative">
+      <button 
+        onClick={onDiscuss} 
+        className="m-0 btn-brand m-0 shadow float-right">Discuss
+      </button>
+    </div>
+  )
+}
+
   return(
     <div className={`quote-list mb-3 p-4 pl-5 d-flex justify-content-between align-items-center ${quote.isSelected ? `active` : ``}`}>
         <div className="select-quote">
             <div className="custom-chekbox">
                 <div className="form-group m-0">
                     <input type="checkbox" id={`check_${quote.id}`} name={`toggleSelect_${quote.id}`} onClick={toggleSelect} value={quote.id} checked={quote.isSelected ? true : false}/>
-                    <label for={`check_${quote.id}`} className="m-0"></label>
+                    { quote.status ===  "ORDER_PLACED" ? 
+                        renderTollTip("Order placed for this design") :
+                      quote.status ===  "PRODUCT_SOLD" ?
+                        renderTollTip("Design is already sold") :
+                      <label for={`check_${quote.id}`} className="m-0"></label>
+                    }
+                    
                 </div>
             </div>
         </div>
@@ -122,7 +164,6 @@ export const QuotedItem = ({quote, index, toggleSelect, search, authUserInfo
                     <div className="info-item">
                         <label className="font-14 text-muted">Fabric details</label>
                         <h5 className="color-333">
-                        {/*'Cotton'*/}
                         <span>{quote.fabricComposition}</span></h5>
                     </div>
                 </div>
@@ -180,7 +221,7 @@ export const QuotedItem = ({quote, index, toggleSelect, search, authUserInfo
         </div>
         {
           quote.status === 'PRICE_GIVEN' ?
-          <div className="quote-price admin-quote-price d-flex flex-column justify-content-center align-items-center">
+            <div className="quote-price admin-quote-price d-flex flex-column justify-content-center align-items-center">
               <div className="text-center">
                   <span className="font-15 valid-till">Price valid till <span className="font-weight-bold"> {getValidDateTill(quote.date, quote.time)}</span> </span>
                   <div className="pricewillbeupdated pt-2 pb-3">
@@ -191,81 +232,72 @@ export const QuotedItem = ({quote, index, toggleSelect, search, authUserInfo
                           <br/>
                           <span className="cursor-pointer font-14">
                           /Piece
-                              <svg className="ml-2" xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16" title={quote.priceInfoText} data-toggle="tooltip" data-placement="top">
-                                <g id="Group_13" data-name="Group 13" transform="translate(1445 -3504)">
-                                  <circle id="Ellipse_1" data-name="Ellipse 1" cx="7" cy="7" r="7" transform="translate(-1445 3506)" fill="#ddd"/>
-                                  <text id="Price_will_be_updated_within_24_hours" data-name="Price will be updated within 24 hours" transform="translate(-1438 3516)" fill="#21242b" font-size="11" font-family="SegoeUI, Segoe UI"><tspan x="-1.332" y="0">i</tspan></text>
-                                </g>
-                              </svg>
+                          {renderTollTip(quote.priceInfoText)}
                           </span>
                       </div>
                   </div>
-                  <div className="position-relative">
-                      <button onClick={onDiscuss} className="m-0 btn-brand m-0 shadow float-right">Discuss</button>
-                      {/*<div className="option">
-                          <div className="dropdown">
-                              <button className="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="6" height="27" viewBox="0 0 6 27">
-                                      <g id="Group_10" data-name="Group 10" transform="translate(1243 -4045)">
-                                          <path id="Path_27893" data-name="Path 27893" d="M22.5,19.5a3,3,0,1,1-3-3A3,3,0,0,1,22.5,19.5Z" transform="translate(-1259.5 4039)" fill="#21242b"/>
-                                          <path id="Path_27894" data-name="Path 27894" d="M22.5,9a3,3,0,1,1-3-3A3,3,0,0,1,22.5,9Z" transform="translate(-1259.5 4039)" fill="#21242b"/>
-                                          <path id="Path_27895" data-name="Path 27895" d="M22.5,30a3,3,0,1,1-3-3A3,3,0,0,1,22.5,30Z" transform="translate(-1259.5 4039)" fill="#21242b"/>
-                                      </g>
-                                  </svg>
-                                  <ul className="dropdown-menu dropdown-menu-right shadow-lg" role="menu" aria-labelledby="menu1">
-                                      <li role="presentation"  className="px-4 pb-3 pt-3"><a role="menuitem" tabindex="-1" href="#" className="font-weight-normal  text-black">Discuss</a></li>
-                                      <li role="presentation"  className="px-4 pb-3"><a role="menuitem" tabindex="-1" href="#" className="font-weight-normal  text-black">Remove</a></li>
-                                  </ul>
-                              </button>
-                          </div>
-                      </div>*/}
-                  </div>
+                  {renderDisscussButton()}
               </div>
           </div> :
+          quote.status === 'PRODUCT_SOLD' ? 
+            <div className="quote-price admin-quote-price d-flex flex-column justify-content-center align-items-center">
+            <div className="text-center">
+            { !quote.price?
+                  <span className="font-14">Price will be updated 
+                  <br/>within <span className="font-italic font-weight-bold">{timeDifference > 0 ? timeDifference : 0} hours</span>
+                    {renderPriceUpdateBox(quote)}
+                    {renderDisscussButton()}
+                  </span> :
+                  <div className="text-center">
+                  <span className="font-15 valid-till">Price valid till <span className="font-weight-bold"> 
+                      {getValidDateTill(quote.date, quote.time)}
+                    </span> 
+                  </span>
+                  <div className="pricewillbeupdated pt-2 pb-3">
+                      <div>
+                          <strong className="font-30">
+                              ${quote.price ? quote.price : `—————`}
+                          </strong>
+                          <br/>
+                          <span className="cursor-pointer font-14">
+                          /Piece
+                          {renderTollTip(quote.priceInfoText)}
+                          </span>
+                      </div>
+                  </div>
+                  {renderDisscussButton()}
+              </div>
+            }
+          </div>
+      </div> :
+          quote.status === 'ORDER_PLACED' ? 
+          <div className="quote-price admin-quote-price d-flex flex-column justify-content-center align-items-center">
+            <div className="text-center">
+            <span className="font-15 valid-till">Price confirmed</span>
+            <br/>
+                <strong className="font-30">${quote.price}</strong>
+                <br/>
+                <span className="cursor-pointer font-14">
+                /Piece
+                  {renderTollTip(quote.priceInfoText)}
+                </span>
+                <div className="pt-3">{renderDisscussButton()}</div>
+            </div>
+          </div>   :
           <div className="quote-price admin-quote-price d-flex flex-column justify-content-center align-items-center">
               <div className="text-center">
-              {
-                quote.status !== 'PRICE_GIVEN' ?
-                <span className="font-14">
-                    Price will be updated <br/>
-                    within <span className="font-italic font-weight-bold">{timeDifference > 0 ? timeDifference : 0} hours</span>
-                </span> :
-                <span className="font-16">
-                    Price will be valid till <span className="font-italic font-weight-bold">{getValidDateTill(quote.date, quote.time)}</span>
-                </span>
-              }
-
-                  <div className="pricewillbeupdated pt-2 pb-3">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="211" height="54" viewBox="0 0 211 54">
-                          <g id="Group_11081" data-name="Group 11081" transform="translate(-1549 -407)">
-                              <g id="Rectangle_6056" data-name="Rectangle 6056" transform="translate(1549 407)" fill="#f4f5f7">
-                                  <path d="M 204 53.5 L 7 53.5 C 3.415894746780396 53.5 0.5 50.5841064453125 0.5 47 L 0.5 7 C 0.5 3.415894746780396 3.415894746780396 0.5 7 0.5 L 204 0.5 C 207.5841064453125 0.5 210.5 3.415894746780396 210.5 7 L 210.5 47 C 210.5 50.5841064453125 207.5841064453125 53.5 204 53.5 Z" stroke="none"/>
-                                  <path d="M 7 1 C 3.69158935546875 1 1 3.691581726074219 1 7 L 1 47 C 1 50.30841827392578 3.69158935546875 53 7 53 L 204 53 C 207.3084106445313 53 210 50.30841827392578 210 47 L 210 7 C 210 3.691581726074219 207.3084106445313 1 204 1 L 7 1 M 7 0 L 204 0 C 207.8659973144531 0 211 3.133998870849609 211 7 L 211 47 C 211 50.86600112915039 207.8659973144531 54 204 54 L 7 54 C 3.134002685546875 54 0 50.86600112915039 0 47 L 0 7 C 0 3.133998870849609 3.134002685546875 0 7 0 Z" stroke="none" fill="#edeff4"/>
-                              </g>
-                              <text id="_-----------" data-name="-----------" transform="translate(1604 441)" fill="#aaacaf" font-size="20" font-family="OpenSans, Open Sans" letter-spacing="0.03em"><tspan x="0" y="0">{quote.price ? quote.price : `—————`}</tspan></text>
-                          </g>
-                      </svg>
-                  </div>
-                  <div className="position-relative">
-                      <button onClick={onDiscuss} className="m-0 btn-brand m-0 shadow float-right">Discuss</button>
-                      {/*<div className="option">
-                          <div className="dropdown">
-                              <button className="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="6" height="27" viewBox="0 0 6 27">
-                                      <g id="Group_10" data-name="Group 10" transform="translate(1243 -4045)">
-                                          <path id="Path_27893" data-name="Path 27893" d="M22.5,19.5a3,3,0,1,1-3-3A3,3,0,0,1,22.5,19.5Z" transform="translate(-1259.5 4039)" fill="#21242b"/>
-                                          <path id="Path_27894" data-name="Path 27894" d="M22.5,9a3,3,0,1,1-3-3A3,3,0,0,1,22.5,9Z" transform="translate(-1259.5 4039)" fill="#21242b"/>
-                                          <path id="Path_27895" data-name="Path 27895" d="M22.5,30a3,3,0,1,1-3-3A3,3,0,0,1,22.5,30Z" transform="translate(-1259.5 4039)" fill="#21242b"/>
-                                      </g>
-                                  </svg>
-                              <ul className="dropdown-menu dropdown-menu-right shadow-lg" role="menu" aria-labelledby="menu1">
-                                  <li role="presentation"  className="px-4 pb-3 pt-3"><a role="menuitem" tabindex="-1" href="#" className="font-weight-normal  text-black">Discuss</a></li>
-                                  <li role="presentation"  className="px-4 pb-3"><a role="menuitem" tabindex="-1" href="#" className="font-weight-normal  text-black">Remove</a></li>
-                              </ul>
-                          </button>
-                          </div>
-                      </div>*/}
-                  </div>
+                {
+                  quote.status !== 'PRICE_GIVEN' ?
+                      <span className="font-14">
+                          Price will be updated <br/>
+                          within <span className="font-italic font-weight-bold">{timeDifference > 0 ? timeDifference : 0} hours</span>
+                      </span> :
+                      <span className="font-16">
+                          Price will be valid till <span className="font-italic font-weight-bold">{getValidDateTill(quote.date, quote.time)}</span>
+                      </span>
+               }
+                  {renderPriceUpdateBox(quote)}
+                  {renderDisscussButton()}
               </div>
           </div>
         }
