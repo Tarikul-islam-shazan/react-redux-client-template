9f5fc20f9528654ca79e6d46811cae68ececb8e6
Md. Rakib Islam <rakibislam.nitex@gmail.com>
2022-01-05 10:21:06 +0600

    login journey design ongoing

--- src/app/buyerLogin/VerifyEmail.js
+++ src/app/buyerLogin/VerifyEmail.js
@@ -36,12 +36,12 @@ const VerifyEmail = () => {
 
     return (
         <LoaderComponent loading={loading}>
-            <div className="questionnaire otp">
+            <div className="questionnaire otp verify-email-address">
                 <div className="questionnaire-form">
-                    <div className="ques-heading">
+                    <div className="ques-heading mb-3">
                         <h2>Verify your Email</h2>
-                        <p className="font-16 my-2">Check your email and click the link to activate your account</p>
-                        <img src={process.env.PUBLIC_URL + "/icons/verifyEmail.png"} alt/>
+                        <p className="font-16 mt-2 mb-3">Check your email and click the link to activate your account</p>
+                        <img src={process.env.PUBLIC_URL + "/icons/verifyEmail.png"} alt className="mb-4"/>
                         <p className="font-16 my-2">If you don't receive the email please check spam</p>
                         <p className="font-16 my-2">OR</p>
                     </div>
