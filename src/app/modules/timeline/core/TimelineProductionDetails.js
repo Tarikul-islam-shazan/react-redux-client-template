4b948e4a43b042b3e526c570d4ebd8148e3beb22
Md. Rakib Islam <rakibislam.nitex@gmail.com>
2022-03-11 18:30:35 +0600

    width ratio changed px to %

--- src/app/modules/timeline/core/TimelineProductionDetails.js
+++ src/app/modules/timeline/core/TimelineProductionDetails.js
@@ -4,11 +4,11 @@ import AllProductionList from "./AllProductionList";
 const TimelineProductionDetails = () => {
     return (
         <>
-            <div className="tab-none">
-                <AllProductionList/>
+            <div className="tab-none one-third">
+                <AllProductionList />
             </div>
         </>
-    )
-}
+    );
+};
 
-export default TimelineProductionDetails
+export default TimelineProductionDetails;
