fbec69fa3e0c7cec00803f682bec2679dead7706
Rashedalam41 <rashed.nitex@gmail.com>
2022-06-21 15:21:16 +0600

    testing husky

--- src/designs/login-journey/Login.js
+++ src/designs/login-journey/Login.js
@@ -1,9 +1,9 @@
 import React from "react";
-const Login = () => {
+const Login = (props) => {
     return (
         <div>
-            <h1>Login Page</h1>
-        </div>
+                                                            <h1>Login Page {props.test}</h1>
+                    </div>
     )
 }
 
