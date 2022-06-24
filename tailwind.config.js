b06422c9181eb296bfeb0e7f32852b8c3b043682
sultan0 <sultanmahmud0@gamil.com>
2022-06-24 21:50:34 +0600

    tailwind config file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            screens: {
+                xs: '360px',
+                sm: '640px',
+                md: '768px',
+                'custom-md': '769px',
+                lg: '1024px',
+                xl: '1280px',
+                '2xl': '1366px',
+                '3xl': '1440px',
+                '4xl': '1536px',
+                '5xl': '1920px'
+            }
+        }
     },
     plugins: []
 }
