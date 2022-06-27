e353f3741a5e29d06c4ea7fb9655021d09345715
Md. Rakib Islam <rakibislam.nitex@gmail.com>
2022-06-27 13:13:53 +0600

    moodboard template created

---
+++ src/assets/designs/moodboard/moodboardTemplate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Boilarplate = () => {
+    //change the Boilarplate name to your specified name
+    return (
+        <div className='container'>
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default Boilarplate //change the Boilarplate name to your specified name
