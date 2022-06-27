1ec6fa1d6b086f405782e1c878d9ea14fa76e935
sultan0 <sultanmahmud0@gamil.com>
2022-06-27 13:16:15 +0600

    home page design update

--- src/assets/designs/home/index.js
+++ src/assets/designs/home/index.js
@@ -916,7 +916,7 @@ const StyleGuide = () => {
                         </div>
                         <div className="py-4">
                             <h4 className="text-xl font-bold text-primaryColor mb-3">Cotton elastane boat neck</h4>
-                            <div className="flex  items-center gap-6 text-base text-primaryColor-shade-100 cursor-pointer">
+                            <div className="flex items-center gap-6 text-base text-primaryColor-shade-100 cursor-pointer">
                                 <span>View Collection</span>
                                 <span>
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -926,7 +926,7 @@ const StyleGuide = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="see-all flex items-center justify-center bg-primaryColor h-[300px] sm:h-[330px] 5xl:h-[456px] cursor-pointer">
+                    <div className="see-all flex items-center justify-center bg-primaryColor hover:bg-black h-[300px] sm:h-[330px] 5xl:h-[456px] cursor-pointer">
                         <div className="text-white-shade-100 text-[40px] text-center flex flex-col items-center">
                             <div>See All</div>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -935,6 +935,89 @@ const StyleGuide = () => {
                         </div>
                     </div>
                 </div>
+
+                <div className="at-a-glance my-14">
+                    <h2 className="text-2xl text-primaryColor mb-5">What <strong>NITEX</strong> can offer</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 tab:grid-cols-3 xl:!grid-cols-4 gap-5">
+                        <div className="glance-box">
+                            <div className="overflow-hidden flex items-center relative h-[250px] 4xl:h-[296px] p-8 bg-white">
+                                <div>
+                                    <div className="text-5xl 3xl:text-6xl text-primaryColor mb-1">100,000+</div>
+                                    <span className="text-lg text-primaryColor uppercase">Styles</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="glance-box">
+                            <div className="overflow-hidden flex items-center relative h-[250px] 4xl:h-[296px] p-8 bg-white">
+                                <div>
+                                    <div className="text-5xl 3xl:text-6xl text-primaryColor mb-1">5,000+</div>
+                                    <span className="text-lg text-primaryColor uppercase">Collections</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="glance-box">
+                            <div className="overflow-hidden flex items-center relative h-[250px] 4xl:h-[296px] p-8 bg-white">
+                                <div>
+                                    <div className="text-5xl 3xl:text-6xl text-primaryColor mb-1">30 Days</div>
+                                    <span className="text-lg text-primaryColor uppercase">Lead Time</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="glance-box">
+                            <div className="overflow-hidden flex items-center relative h-[250px] 4xl:h-[296px] p-8 bg-white">
+                                <div>
+                                    <div className="text-5xl 3xl:text-6xl text-primaryColor mb-1">10,000+</div>
+                                    <span className="text-lg text-primaryColor uppercase">Materials in use</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="glance-box">
+                            <div className="overflow-hidden flex items-center relative h-[250px] 4xl:h-[296px] p-8 bg-white">
+                                <div>
+                                    <div className="text-5xl 3xl:text-6xl text-primaryColor mb-1">20+</div>
+                                    <span className="text-lg text-primaryColor uppercase">Categories</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="glance-box">
+                            <div className="overflow-hidden flex items-center relative h-[250px] 4xl:h-[296px] p-8 bg-white">
+                                <div>
+                                    <div className="text-5xl 3xl:text-6xl text-primaryColor mb-1">13</div>
+                                    <span className="text-lg text-primaryColor uppercase">Seasons</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="glance-box">
+                            <div className="overflow-hidden flex items-center relative h-[250px] 4xl:h-[296px] p-8 bg-white">
+                                <div>
+                                    <div className="text-5xl 3xl:text-6xl text-primaryColor mb-1">30 Days</div>
+                                    <span className="text-lg text-primaryColor uppercase">Lead Time</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="see-all flex items-center justify-center bg-primaryColor hover:bg-black h-[250px] 4xl:h-[296px] cursor-pointer">
+                            <div className="text-white-shade-100 text-[40px] text-center flex flex-col items-center">
+                                <div>Dashboard</div>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M21 12L14 19M3 12H21H3ZM21 12L14 5L21 12Z" stroke="#F5F5F5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="belong-here flex items-center justify-center relative">
+                    <div className="text-[rgba(33, 36, 43, 0.1)] text-[200px] font-bold absolute">Belong</div>
+                    <iframe width="930" height="620" src="https://www.youtube.com/embed/QjisC1Aj-rA"
+                            title="FLYING OVER NEW ZEALAND (4K UHD) - Calming Music With Spectacular Natural Landscape For Relaxation"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen>
+                    </iframe>
+
+                    <div>Here</div>
+                </div>
+
 
             </div>
 
