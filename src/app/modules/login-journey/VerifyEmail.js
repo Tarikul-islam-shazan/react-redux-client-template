a45a2be9de8f151f7e0aa9bcca55bd954db920c4
Rashedalam41 <rashed.nitex@gmail.com>
2022-06-28 12:58:51 +0600

    add default layout and home page html content

--- src/app/modules/login-journey/VerifyEmail.js
+++ src/app/modules/login-journey/VerifyEmail.js
@@ -1,10 +1,10 @@
-import React, {useEffect} from 'react';
-import {useNavigate, useSearchParams} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import Http from '../../services/Http';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 
 const VerifyEmail = () => {
-    const [searchParams,] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -13,12 +13,12 @@ const VerifyEmail = () => {
             .then((res) => {
                 Http.GET('userInfo')
                     .then(async (response) => {
-                        let {data} = response;
+                        let { data } = response;
                         await localStorage.setItem('userInfo', JSON.stringify(data));
                         toast.success('Verification Successful!')
                         await navigate('/activation')
                     })
-                    .catch(({response}) => {
+                    .catch(({ response }) => {
                         toast.error(response.data.message)
                     });
             })
