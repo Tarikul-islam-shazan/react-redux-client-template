4ef4eabde95f5656dadb3bb0d246f12a2a820265
Rashedalam41 <rashed.nitex@gmail.com>
2022-07-06 15:42:14 +0600

    modify default layout

--- src/app/layouts/DefaultLayout.js
+++ src/app/layouts/DefaultLayout.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { ReactComponent as MenuIcon } from '../../assets/images/home/humbargerMenu.svg'
 import { ReactComponent as NitexIcon } from '../../assets/images/home/nitexLogo.svg'
 import { ReactComponent as CloseIcon } from '../../assets/images/home/cross.svg'
 import { ReactComponent as IconFavourite } from '../../assets/images/home/favourite.svg'
 import { ReactComponent as IconNotification } from '../../assets/images/home/notification.svg'
-import Slide1 from '../../assets/images/home/slide1.png';
 import LoaderComponent from '../common/LoaderComponent'
 import { useSelector } from 'react-redux'
 import { authUserInfo } from '../services/Util'
@@ -14,6 +13,27 @@ const DefaultLayout = () => {
   const navigate = useNavigate()
   const currentLocation = useLocation()
   const loaderState = useSelector(state => state.loader)
+  const [showCollectionSubMenu, setShowCollectionSubMenu] = useState(false)
+  const [showFavouriteSubmenu, setShowFavouriteSubmenu] = useState(false)
+  const [activeMenu, setActiveMenu] = useState('dashboard')
+
+  useEffect(() => {
+    let firstSegment = currentLocation.pathname.split('/')[1]
+    switch (firstSegment) {
+      case 'dashboard':
+        setActiveMenu('dashboard')
+        break
+      case 'orders':
+        setActiveMenu('orders')
+        break
+      case 'moodboard':
+        setActiveMenu('moodboard')
+        break
+      default:
+        setActiveMenu('dashboard')
+        break
+    }
+  }, [currentLocation])
 
   useEffect(() => {
     let data = JSON.parse(localStorage.getItem('userInfo'))
@@ -38,6 +58,77 @@ const DefaultLayout = () => {
     }
   }, [])
 
+  const renderCollectionSubmenu = () => {
+    if (showCollectionSubMenu) {
+      return (
+        <div
+          className='submenu flex flex-col sm:flex-row p-5 bg-white shadow-lg w-[1240px] sm:h-[420px]  absolute top-[58px] left-[50%] translate-x-[-50%] z-[999]'>
+          <div className='sm:w-1/2 block bg-white-shade-100'>
+            <div className='overflow-hidden w-full h-full '>
+              {/*<img src='' alt='' className='w-full h-full object-cover'/>*/}
+            </div>
+          </div>
+          <div className='sm:w-1/2'>
+            <div className='uppercase text-[60px] text-white-shade-100 font-bold mt-8 ml-[-10px]'>COLLECTIONS</div>
+            <div className='space-y-10 p-4 mt-4 menu-items'>
+              <a href='#' className='active'>
+                <div className='flex gap-10'>
+                  <span>NITEX COLLECTION</span>
+                  <span>/ 07</span>
+                </div>
+              </a>
+              <a href='#'>
+                <div className='flex gap-10'>
+                  <span>MY COLLECTION</span>
+                  <span>/ 07</span>
+                </div>
+              </a>
+              <a href='#'>
+                <div className='flex gap-10'>
+                  <span>COLLECTION REQUESTS</span>
+                  <span>/ 07</span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  const renderFavouriteSubmenu = () => {
+    if (showFavouriteSubmenu) {
+      return (
+        <div
+          className='submenu flex flex-col sm:flex-row p-6 bg-white shadow-lg w-[455px] sm:h-[386px]  absolute top-[66px] right-0  z-[999]'>
+          <div>
+            <div className='uppercase text-[50px] text-white-shade-100 font-bold'>Favorites</div>
+            <div className='menu-items px-8'>
+              <a href='#' className='active'>
+                <div className='flex gap-10'>
+                  <span>Moodboard </span>
+                  <span>/ 07</span>
+                </div>
+              </a>
+              <a href='#'>
+                <div className='flex gap-10'>
+                  <span>COLLECTION</span>
+                  <span>/ 07</span>
+                </div>
+              </a>
+              <a href='#'>
+                <div className='flex gap-10'>
+                  <span>Styles</span>
+                  <span>/ 07</span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+
   return (
     <LoaderComponent loading={loaderState}>
       <div className='container-fluid bg-primaryColor-shade-300'>
@@ -60,44 +151,15 @@ const DefaultLayout = () => {
           </div>
           <div className='main-menu hidden xl:block ml-16 4xl:ml-28'>
             <ul>
-              <li className='text-base text-primaryColor uppercase inline-block mr-6 4xl:mr-10 5xl:mr-14 active'>
+              <li className={`text-base text-primaryColor uppercase inline-block mr-6 4xl:mr-10 5xl:mr-14 ${activeMenu === 'dashboard' ? 'active' : ''}`}>
                 <Link to='/dashboard'>Home</Link>
               </li>
-              <li className='text-base text-primaryColor uppercase inline-block mr-6 4xl:mr-10 5xl:mr-14'>
+              <li className={`text-base text-primaryColor uppercase inline-block mr-6 4xl:mr-10 5xl:mr-14 ${activeMenu === 'moodboard' ? 'active' : ''}`}>
                 <Link to='/moodboard'>Moodboard</Link>
               </li>
               <li className='text-base text-primaryColor uppercase inline-block mr-6 4xl:mr-10 5xl:mr-14 relative'>
-                <a href='#'>Collections</a>
-                {/*<div className='submenu flex flex-col sm:flex-row p-5 bg-white shadow-lg w-[1240px] sm:h-[420px]  absolute top-[58px] left-[50%] translate-x-[-50%] z-[999]'>*/}
-                {/*    <div className='sm:w-1/2 block bg-white-shade-100'>*/}
-                {/*      <div className='overflow-hidden w-full h-full '>*/}
-                {/*        /!*<img src='' alt='' className='w-full h-full object-cover'/>*!/*/}
-                {/*      </div>*/}
-                {/*    </div>*/}
-                {/*    <div className='sm:w-1/2'>*/}
-                {/*      <div className='uppercase text-[60px] text-white-shade-100 font-bold mt-8 ml-[-10px]'>COLLECTIONS</div>*/}
-                {/*      <div className='space-y-10 p-4 mt-4 menu-items'>*/}
-                {/*        <a href='#' className='active'>*/}
-                {/*          <div className='flex gap-10'>*/}
-                {/*            <span>NITEX COLLECTION</span>*/}
-                {/*            <span>/ 07</span>*/}
-                {/*          </div>*/}
-                {/*        </a>*/}
-                {/*        <a href='#'>*/}
-                {/*          <div className='flex gap-10'>*/}
-                {/*            <span>MY COLLECTION</span>*/}
-                {/*            <span>/ 07</span>*/}
-                {/*          </div>*/}
-                {/*        </a>*/}
-                {/*        <a href='#'>*/}
-                {/*          <div className='flex gap-10'>*/}
-                {/*            <span>COLLECTION REQUESTS</span>*/}
-                {/*            <span>/ 07</span>*/}
-                {/*          </div>*/}
-                {/*        </a>*/}
-                {/*      </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                <a href='#' onClick={() => setShowCollectionSubMenu(prevState => !prevState)}>Collections</a>
+                {renderCollectionSubmenu()}
               </li>
               <li className='text-base text-primaryColor uppercase inline-block mr-6 4xl:mr-10 5xl:mr-14'>
                 <a href='#'>Quotes</a>
@@ -105,7 +167,7 @@ const DefaultLayout = () => {
               <li className='text-base text-primaryColor uppercase inline-block mr-6 4xl:mr-10 5xl:mr-14'>
                 <a href='#'>Samples</a>
               </li>
-              <li className='text-base text-primaryColor uppercase inline-block mr-6 4xl:mr-10 5xl:mr-14'>
+              <li className={`text-base text-primaryColor uppercase inline-block mr-6 4xl:mr-10 5xl:mr-14 ${activeMenu === 'orders' ? 'active' : ''}`}>
                 <Link to='/orders/my-orders'>Orders</Link>
               </li>
               <li className='text-base text-primaryColor uppercase inline-block mr-6 4xl:mr-10 5xl:mr-14'>
@@ -114,12 +176,15 @@ const DefaultLayout = () => {
             </ul>
           </div>
 
-          <div className='user-interaction ml-auto flex items-center gap-x-6'>
-            <div className='w-[40px] h-[40px] rounded-full bg-primaryColor-shade-300 p-2 relative flex items-center justify-center cursor-pointer'>
+          <div className='user-interaction ml-auto flex items-center gap-x-6'
+               onClick={() => setShowFavouriteSubmenu(prevState => !prevState)}>
+            <div
+              className='w-[40px] h-[40px] rounded-full bg-primaryColor-shade-300 p-2 relative flex items-center justify-center cursor-pointer'>
               <span>
                   <IconFavourite />
               </span>
-              <span className='absolute top-[-9px] right-[-22px] text-sm text-white-shade-100 bg-primaryColor inline-block rounded-[20px] px-3 text-center'>
+              <span
+                className='absolute top-[-9px] right-[-22px] text-sm text-white-shade-100 bg-primaryColor inline-block rounded-[20px] px-3 text-center'>
                   12
               </span>
             </div>
@@ -128,37 +193,15 @@ const DefaultLayout = () => {
                   <span>
                     <IconNotification />
                   </span>
-                  <span className='absolute top-[-9px] right-[-22px] text-sm text-white-shade-100 bg-primaryColor inline-block rounded-[20px] px-3 text-center'>
+                <span
+                  className='absolute top-[-9px] right-[-22px] text-sm text-white-shade-100 bg-primaryColor inline-block rounded-[20px] px-3 text-center'>
                     99+
                 </span>
               </div>
-              {/*<div className='submenu flex flex-col sm:flex-row p-6 bg-white shadow-lg w-[455px] sm:h-[386px]  absolute top-[66px] right-0  z-[999]'>*/}
-              {/*  <div>*/}
-              {/*    <div className='uppercase text-[50px] text-white-shade-100 font-bold'>Favorites</div>*/}
-              {/*    <div className='menu-items px-8'>*/}
-              {/*      <a href='#' className='active'>*/}
-              {/*        <div className='flex gap-10'>*/}
-              {/*          <span>Moodboard </span>*/}
-              {/*          <span>/ 07</span>*/}
-              {/*        </div>*/}
-              {/*      </a>*/}
-              {/*      <a href='#'>*/}
-              {/*        <div className='flex gap-10'>*/}
-              {/*          <span>COLLECTION</span>*/}
-              {/*          <span>/ 07</span>*/}
-              {/*        </div>*/}
-              {/*      </a>*/}
-              {/*      <a href='#'>*/}
-              {/*        <div className='flex gap-10'>*/}
-              {/*          <span>Styles</span>*/}
-              {/*          <span>/ 07</span>*/}
-              {/*        </div>*/}
-              {/*      </a>*/}
-              {/*    </div>*/}
-              {/*  </div>*/}
-              {/*</div>*/}
+              {renderFavouriteSubmenu()}
             </div>
-            <div className='w-[40px] h-[40px] rounded-full bg-primaryColor-shade-300 relative border border-white-shade-100 flex items-center justify-center cursor-pointer overflow-hidden'>
+            <div
+              className='w-[40px] h-[40px] rounded-full bg-primaryColor-shade-300 relative border border-white-shade-100 flex items-center justify-center cursor-pointer overflow-hidden'>
               {!authUserInfo()?.profilePicDocument?.docUrl && <img
                 src='./images/user.jpg'
                 className='object-cover object-top w-full h-full'
