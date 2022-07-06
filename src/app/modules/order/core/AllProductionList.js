e0feb436a736dae94d9866df30e45103104344f2
Rashedalam41 <rashed.nitex@gmail.com>
2022-07-06 11:11:42 +0600

    solve taskManage modal rendering issue

--- src/app/modules/order/core/AllProductionList.js
+++ src/app/modules/order/core/AllProductionList.js
@@ -6,229 +6,231 @@ import Tooltip from '@mui/material/Tooltip'
 import TaskManage from '../../../common/TaskManage'
 
 const AllProductionList = () => {
-    const timelineStore = useSelector((store) => store.timeline)
-    const [showTaskDetailsModal, setShowTaskDetailsModal] = useState(false)
-    const [selectedId, setSelectedId] = useState(null)
-    const params = useParams()
+  const timelineStore = useSelector((store) => store.timeline)
+  const [showTaskDetailsModal, setShowTaskDetailsModal] = useState(false)
+  const [selectedId, setSelectedId] = useState(null)
+  const params = useParams()
 
-    const renderStepIcon = (sample) => {
-        if (
-            sample.formattedTaskStatus === 'APPROVED' ||
-            sample.formattedTaskStatus === 'LATE_APPROVED' ||
-            sample.status === 'SCOPE_OFF'
-        ) {
-            return '/icons/Completed-icon.svg'
-        } else if (sample.formattedTaskStatus === 'EXPIRED') {
-            return '/icons/Due-icon.svg'
-        } else {
-            return '/icons/Pending-icon.svg'
-        }
+  const renderStepIcon = (sample) => {
+    if (
+      sample.formattedTaskStatus === 'APPROVED' ||
+      sample.formattedTaskStatus === 'LATE_APPROVED' ||
+      sample.status === 'SCOPE_OFF'
+    ) {
+      return '/icons/Completed-icon.svg'
+    } else if (sample.formattedTaskStatus === 'EXPIRED') {
+      return '/icons/Due-icon.svg'
+    } else {
+      return '/icons/Pending-icon.svg'
     }
+  }
 
-    const renderStepClass = (sample) => {
-        if (
-            sample.formattedTaskStatus === 'APPROVED' ||
-            sample.formattedTaskStatus === 'LATE_APPROVED' ||
-            sample.status === 'SCOPE_OFF'
-        ) {
-            return 'completed'
-        } else if (sample.formattedTaskStatus === 'EXPIRED') {
-            return 'due'
-        } else {
-            return 'pending'
-        }
+  const renderStepClass = (sample) => {
+    if (
+      sample.formattedTaskStatus === 'APPROVED' ||
+      sample.formattedTaskStatus === 'LATE_APPROVED' ||
+      sample.status === 'SCOPE_OFF'
+    ) {
+      return 'completed'
+    } else if (sample.formattedTaskStatus === 'EXPIRED') {
+      return 'due'
+    } else {
+      return 'pending'
     }
+  }
 
-    const handleTaskManager = (sample) => {
-        setSelectedId(sample.id)
-        setShowTaskDetailsModal(true)
+  const handleTaskManager = (sample) => {
+    setSelectedId(sample.id)
+    setShowTaskDetailsModal(true)
+  }
+
+  const renderTaskDate = (sample) => {
+    if (sample.formattedTaskStatus === 'EXPIRED') {
+      return '+' + sample.dateOver
+    } else if ('actualEndDate' in sample) {
+      return changeDateFormat(
+        sample.actualEndDate,
+        'YYYY-MM-DD',
+        'DD-MMM'
+      )
+    } else {
+      return changeDateFormat(sample.endDate, 'YYYY-MM-DD', 'DD-MMM')
     }
+  }
 
-    const renderTaskDate = (sample) => {
-        if (sample.formattedTaskStatus === 'EXPIRED') {
-            return '+' + sample.dateOver
-        } else if ('actualEndDate' in sample) {
-            return changeDateFormat(
-                sample.actualEndDate,
-                'YYYY-MM-DD',
-                'DD-MMM'
-            )
-        } else {
-            return changeDateFormat(sample.endDate, 'YYYY-MM-DD', 'DD-MMM')
-        }
-    }
+  const renderSamplingStepList = (list) => {
+    return list?.map((sample, index) => {
+      return (
+        <div
+          className={`single-task ${renderStepClass(sample)}`}
+          key={`sample_index_${index}`}
+          onClick={() => handleTaskManager(sample)}
+          data-bs-target='#taskManageModal'
+          data-bs-toggle='modal'
+        >
+          <div className='task-name'>
+            <img src={renderStepIcon(sample)} alt='complete' />
+            <Tooltip
+              title={sample.stepName}
+              placement={'top'}
+              arrow
+            >
+              <span>{getShortName(sample.stepName, 25)}</span>
+            </Tooltip>
+          </div>
+          <div className='date-details'>
+            <span>{renderTaskDate(sample)}</span>
+          </div>
+        </div>
+      )
+    })
+  }
 
-    const renderSamplingStepList = (list) => {
-        return list?.map((sample, index) => {
-            return (
-                <div
-                    className={`single-task ${renderStepClass(sample)}`}
-                    key={`sample_index_${index}`}
-                    onClick={() => handleTaskManager(sample)}
-                    data-bs-target='#taskManageModal'
-                    data-bs-toggle='modal'
-                >
-                    <div className='task-name'>
-                        <img src={renderStepIcon(sample)} alt='complete' />
-                        <Tooltip
-                            title={sample.stepName}
-                            placement={'top'}
-                            arrow
-                        >
-                            <span>{getShortName(sample.stepName, 25)}</span>
-                        </Tooltip>
-                    </div>
-                    <div className='date-details'>
-                        <span>{renderTaskDate(sample)}</span>
-                    </div>
-                </div>
-            )
-        })
-    }
-
-    return (
-        <>
-            <div className='one-third all-production-details'>
-                <div className='factory-view relative'>
-                    <h5 className='absolute left-5 top-5'>Factory</h5>
-                    <img src='/images/factory-view.jpg' />
-                    <button
-                        className='btn bg-[#F5F5F5] text-primaryColor absolute right-5 bottom-5 w-[80px] p-4'
-                        data-bs-toggle='modal'
-                        data-bs-target='#InviteTeamMember'
-                    >
-                        360<sup>0</sup>{' '}
-                    </button>
-                </div>
-                <div className='text-tc'>
+  return (
+    <>
+      <div className='one-third all-production-details'>
+        <div className='factory-view relative'>
+          <h5 className='absolute left-5 top-5'>Factory</h5>
+          <img src='/images/factory-view.jpg' />
+          <button
+            className='btn bg-[#F5F5F5] text-primaryColor absolute right-5 bottom-5 w-[80px] p-4'
+            data-bs-toggle='modal'
+            data-bs-target='#InviteTeamMember'
+          >
+            360<sup>0</sup>{' '}
+          </button>
+        </div>
+        <div className='text-tc'>
                     <span className='regular-14 gray_dark_02'>
                         Time & Action /Critical Dates
                     </span>
+        </div>
+        <div className='production-accordion'>
+          <div className='accordion' id='accordionExample'>
+            <div className='accordion-item'>
+              <div
+                className='accordion-header'
+                id='headingSampling'
+              >
+                <h5 className='mb-0'>
+                  <button
+                    className='accordion-button bg-white btn btn-link'
+                    data-bs-toggle='collapse'
+                    data-bs-target='#collapseSampling'
+                    aria-expanded='true'
+                    aria-controls='collapseSampling'
+                  >
+                    Sampling
+                  </button>
+                </h5>
+              </div>
+              <div
+                id='collapseSampling'
+                className='accordion-collapse bg-white collapse show'
+                aria-labelledby='headingSampling'
+                data-bs-parent='#accordionExample'
+              >
+                <div className='card-body'>
+                  <div className='all-task-status'>
+                    {renderSamplingStepList(
+                      timelineStore?.stepList?.SAMPLING
+                    )}
+                  </div>
                 </div>
-                <div className='production-accordion'>
-                    <div className='accordion' id='accordionExample'>
-                        <div className='accordion-item'>
-                            <div
-                                className='accordion-header'
-                                id='headingSampling'
-                            >
-                                <h5 className='mb-0'>
-                                    <button
-                                        className='accordion-button bg-white btn btn-link'
-                                        data-bs-toggle='collapse'
-                                        data-bs-target='#collapseSampling'
-                                        aria-expanded='true'
-                                        aria-controls='collapseSampling'
-                                    >
-                                        Sampling
-                                    </button>
-                                </h5>
-                            </div>
-                            <div
-                                id='collapseSampling'
-                                className='accordion-collapse bg-white collapse show'
-                                aria-labelledby='headingSampling'
-                                data-bs-parent='#accordionExample'
-                            >
-                                <div className='card-body'>
-                                    <div className='all-task-status'>
-                                        {renderSamplingStepList(
-                                            timelineStore?.stepList?.SAMPLING
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='accordion-item'>
-                            <div
-                                className='accordion-header'
-                                id='headingProduction'
-                            >
-                                <h5 className='mb-0'>
-                                    <button
-                                        className='accordion-button bg-white btn btn-link collapsed'
-                                        data-bs-toggle='collapse'
-                                        data-bs-target='#collapseProduction'
-                                        aria-expanded='true'
-                                        aria-controls='collapseProduction'
-                                    >
-                                        Production
-                                    </button>
-                                </h5>
-                            </div>
-                            <div
-                                id='collapseProduction'
-                                className='accordion-collapse bg-white collapse'
-                                aria-labelledby='headingProduction'
-                                data-bs-parent='#accordionExample'
-                            >
-                                <div className='card-body'>
-                                    <div className='all-task-status'>
-                                        {renderSamplingStepList(
-                                            timelineStore?.stepList?.PRODUCTION
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='accordion-item'>
-                            <div
-                                className='accordion-header'
-                                id='headingInspection'
-                            >
-                                <h5 className='mb-0'>
-                                    <button
-                                        className='accordion-button bg-white btn btn-link collapsed'
-                                        data-bs-toggle='collapse'
-                                        data-bs-target='#collapseInspection'
-                                        aria-expanded='true'
-                                        aria-controls='collapseInspection'
-                                    >
-                                        Inspection
-                                    </button>
-                                </h5>
-                            </div>
-                            <div
-                                id='collapseInspection'
-                                className='accordion-collapse bg-white collapse'
-                                aria-labelledby='headingInspection'
-                                data-bs-parent='#accordionExample'
-                            >
-                                <div className='card-body'>
-                                    <div className='all-task-status'>
-                                        {renderSamplingStepList(
-                                            timelineStore?.stepList?.INSPECTION
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
+            <div className='accordion-item'>
+              <div
+                className='accordion-header'
+                id='headingProduction'
+              >
+                <h5 className='mb-0'>
+                  <button
+                    className='accordion-button bg-white btn btn-link collapsed'
+                    data-bs-toggle='collapse'
+                    data-bs-target='#collapseProduction'
+                    aria-expanded='true'
+                    aria-controls='collapseProduction'
+                  >
+                    Production
+                  </button>
+                </h5>
+              </div>
+              <div
+                id='collapseProduction'
+                className='accordion-collapse bg-white collapse'
+                aria-labelledby='headingProduction'
+                data-bs-parent='#accordionExample'
+              >
+                <div className='card-body'>
+                  <div className='all-task-status'>
+                    {renderSamplingStepList(
+                      timelineStore?.stepList?.PRODUCTION
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='accordion-item'>
+              <div
+                className='accordion-header'
+                id='headingInspection'
+              >
+                <h5 className='mb-0'>
+                  <button
+                    className='accordion-button bg-white btn btn-link collapsed'
+                    data-bs-toggle='collapse'
+                    data-bs-target='#collapseInspection'
+                    aria-expanded='true'
+                    aria-controls='collapseInspection'
+                  >
+                    Inspection
+                  </button>
+                </h5>
+              </div>
+              <div
+                id='collapseInspection'
+                className='accordion-collapse bg-white collapse'
+                aria-labelledby='headingInspection'
+                data-bs-parent='#accordionExample'
+              >
+                <div className='card-body'>
+                  <div className='all-task-status'>
+                    {renderSamplingStepList(
+                      timelineStore?.stepList?.INSPECTION
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-            <div
-                className='modal task-conversation fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto'
-                id='taskManageModal'
-                tabIndex='-1'
-                aria-labelledby='exampleModalCenterTitle'
-                aria-modal='true'
-                role='dialog'
-            >
-                <div className='modal-dialog max-w-[485px] mr-0 mt-0 modal-dialog-centered relative w-auto pointer-events-none'>
-                    <div className='modal-content h-screen border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding outline-none text-current'>
-                        <TaskManage
-                            timelinePanel={true}
-                            id={selectedId}
-                            orderId={params.orderId}
-                            closeModal={() => setShowTaskDetailsModal(false)}
-                            callback={() => false}
-                        />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+      <div
+        className='modal task-conversation fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto'
+        id='taskManageModal'
+        tabIndex='-1'
+        aria-labelledby='exampleModalCenterTitle'
+        aria-modal='true'
+        role='dialog'
+        onBlur={() => setShowTaskDetailsModal(false)}
+      >
+        <div className='modal-dialog max-w-[485px] mr-0 mt-0 modal-dialog-centered relative w-auto pointer-events-none'>
+          <div
+            className='modal-content h-screen border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding outline-none text-current'>
+            {showTaskDetailsModal && <TaskManage
+              timelinePanel={true}
+              id={selectedId}
+              orderId={params.orderId}
+              closeModal={() => setShowTaskDetailsModal(false)}
+              callback={() => false}
+            />}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default AllProductionList
